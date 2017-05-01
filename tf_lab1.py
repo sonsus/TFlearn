efaadbc327a822e7d5de6e2ad4f406afcbd60aec
SEONIL <seonilson@gmail.com>
2017-05-01 05:44:32 -0400

    lab1 in the middle

---
+++ tf_lab1.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+hello=tf.constant("hello sungkim")
+
+sess=tf.Session()
+
+print("\n\n%s\n"%sess.run(hello))
+
+
+node1=tf.constant(3.0, tf.float32)
+node2=tf.constant(4.0) # tf.float32 is implicitly applied here
+node3=tf.add(node1,node2)
+
+print("node1: ", node1) 
+print("node2: ", node2)
+print("node3: ", node3)
+
+sess=tf.Session()
+print("session.run(node1,node2): ", sess.run([node1,node2]))
+print("session.run(node3): ", sess.run(node3))
+
+a=tf.placeholder(tf.float32)
+b=tf.placeholder(tf.float32)
+
+adder_node=a+b
+
+print(sess.run(adder_node, feed_dict={a:3, b:4.5, }))
+print(sess.run(adder_node, feed_dict={a:[1,3], b:[2,4], }))
+
+sess.close()
