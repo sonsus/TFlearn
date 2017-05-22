1bca09ba57877f56b1f4fe3727d81196c4c2e792
SEONIL <seonilson92@gmail.com>
2017-05-23 00:47:42 +0900

    lab4 dataloading corrected so that it works(incomplete.py) queue runner ex and somuch left here

--- lab4_incomplete.py
+++ lab4_incomplete.py
@@ -1,20 +1,16 @@
 #matrices are the chosen ones.,
 import tensorflow as tf 
+import numpy as np
+tf.set_random_seed(777) #for reproducibility
 
-x_data=[
-[73.,80.,75.,],
-[93.,88.,93.,],
-[89.,91.,90.,],
-[96.,98.,100.,],
-[73.,66.,70.,],
-]
-y_data=[
-[152.],[185.],[180.],[196.],[142.],
-]
+xy=np.loadtxt("lab4_dat_1.csv", delimiter=",", dtype=np.float32)
+x_data=xy[:,:3]
+y_data=xy[:,[3]]
 
 #placeholders for a tensor that will always be fed.
-X=tf.placeholder(tf.float32 , shape=None)
-Y=tf.placeholder(tf.float32 , shape=None)
+X=tf.placeholder(tf.float32 , shape=[None,3])
+Y=tf.placeholder(tf.float32 , shape=[None,1])
+#[None,1] means unlimited rows and 3 columns
 W=tf.Variable(tf.random_normal([3,1]), name="weight")
 b=tf.Variable(tf.random_normal([1]), name="bias")
 
@@ -34,3 +30,9 @@ for step in range(2001):
     cost_val, hy_val, _=sess.run([cost,hypothesis,train], feed_dict={X:x_data, Y:y_data})
     if step%10==0:
         print(step, "cost=", cost_val, "\nprediction\n", hy_val)
+
+print("your score: ", sess.run(hypothesis, 
+						feed_dict={X: [[100,70,101]]}))
+
+print("others' scores: ", sess.run(hypothesis, 
+						feed_dict={X: [[60,70,110],[90,100,80]]}))  
